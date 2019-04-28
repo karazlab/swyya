2d4b2234e26ab2f67846401d1369e50f983c2789
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-04-28 22:27:02 +0300

    Fixed issue with current_user on showing project page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,6 +4,7 @@ module ProjectsHelper
   end
 
   def project_liked?
+    return false unless user_signed_in?
     current_user.voted_for?(@project) && @project.get_upvotes.where(voter: current_user).present?
   end
 end
