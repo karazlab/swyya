dce9354914196d4281c0d067a3bdd58561d8745d
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-03 02:18:04 +0300

    Changed the way to view images

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -7,4 +7,8 @@ module ProjectsHelper
     return false unless user_signed_in?
     current_user.voted_for?(@project) && @project.get_upvotes.where(voter: current_user).present?
   end
+
+  def project_image(project)
+    project.image_url.present? ? project.image_url : asset_path('missing.png')
+  end
 end
