eac59e535204099b92d41745ba0bb5a42c7b1f4f
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-03 23:06:44 +0300

    Ideas ROutes & Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :projects do
-    member do
-      post 'like'
-      post 'dislike'
-      post 'hide'
-      post 'active'
+  %i[ideas projects].each do |module_controller|
+    resources module_controller do
+      member do
+        post 'like'
+        post 'dislike'
+        post 'hide'
+        post 'active'
+      end
     end
   end
-
+  
   root 'visitors#index'
 
   get '/board', to: 'boards#index', as: 'user_board'
