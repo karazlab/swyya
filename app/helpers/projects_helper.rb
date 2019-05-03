5a45097f723cb1a1de700954f6fb9d402f4e106f
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-03 23:36:53 +0300

    Ideas Views & Adding it to User Board, Changed Projects Views

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,13 +1,4 @@
 module ProjectsHelper
-  def user_information(project)
-    project.user_username || project.user_email
-  end
-
-  def project_liked?
-    return false unless user_signed_in?
-    current_user.voted_for?(@project) && @project.get_upvotes.where(voter: current_user).present?
-  end
-
   def project_image(project)
     project.image_url.present? ? project.image_url : asset_path('missing.png')
   end
