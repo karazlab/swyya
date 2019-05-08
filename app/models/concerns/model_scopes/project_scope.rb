285d3f802eb9042726d86de8bcc2bbf82920c731
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-09 02:06:08 +0300

    Seachable Tags

--- app/models/concerns/model_scopes/project_scope.rb
+++ app/models/concerns/model_scopes/project_scope.rb
@@ -3,9 +3,17 @@ module ModelScopes::ProjectScope
 
   included do
     send(:include, Module.const_get('ModelScopes::SharedScope'))
-    scope :searchable, ->(search) {
+    scope :contentbase, ->(search) {
       active.where('title LIKE :search OR description LIKE :search',
                    search: "%#{search.downcase}%")
+    }
+
+    scope :tagged, ->(search) {
+      active.tagged_with(search, on: :tech_stacks)
+    }
+
+    scope :searchable, ->(search) {
+      contentbase(search) + tagged(search)
     }
   end
 end
