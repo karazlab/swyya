a852d047873222e50ce57d1588d91082b47a3657
Abd El Rahman HSN <35659009+Abd-El-Rahman-HSN@users.noreply.github.com>
2019-05-15 01:46:55 +0200

    Update app/controllers/callbacks_controller.rb
    Co-Authored-By: Elayan <elayan.hr@gmail.com>

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.from_omniauth(request.env["omniauth.auth"])    
     sign_in_and_redirect user
-    flash[:notice] = "Signed in successfully."
+    flash[:notice] = t(:sign_in_message)
   end
 end
