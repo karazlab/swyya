9cd3f977c91c05abefc94fac691826782dcbda08
ehamamrah <elayan.hr@gmail.com>
2020-02-27 02:34:22 +0200

    Changes for PROD

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'labkaraz@gmail.com'
+  config.mailer_sender = 'hello@sawia.xyz'
 
   config.omniauth :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: 'user:email'
 
