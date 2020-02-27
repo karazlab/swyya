3961cdaffa863deaaedefbbb48f997ef5e62a87c
ehamamrah <elayan.hr@gmail.com>
2020-02-27 19:26:49 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, "git@github.com:karazlab/swyya.git"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "vendor/bundle"
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
 
-set :nginx_server_name, "localhost #{fetch(:application)}.local stg.shiftmanager.visunware.com"
+set :nginx_server_name, "localhost #{fetch(:application)}.local sawia.xyz"
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.6.0@shift-manager'
