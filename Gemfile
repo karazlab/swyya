54f1eec664ea1b7037530673e3fdc8439e8a70cc
ehamamrah <elayan.hr@gmail.com>
2020-02-28 02:50:45 +0200

    CAP

--- Gemfile
+++ Gemfile
@@ -60,3 +60,8 @@ gem 'newrelic_rpm'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'capistrano', '~> 3.11'
+gem 'capistrano-rails', '~> 1.4'
+gem 'capistrano-passenger', '~> 0.2.0'
+gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.12.0"
+set :application, "myapp"
+set :repo_url, "git@github.com:username/myapp.git"
+set :branch, "production"
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
+# This is useful if you don't want to use ENV variables
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
